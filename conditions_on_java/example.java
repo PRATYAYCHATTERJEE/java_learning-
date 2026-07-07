b40fdac087b00fe4df16c8b80b456c7a62c0b879
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2026-07-07 22:49:25 +0530

    Adding More Code

---
+++ conditions_on_java/example.java
@@ -0,0 +1,38 @@
+package conditions_on_java;
+/* 
+public class example {                                            ( while loop )
+    public static void main(String[] args) {
+
+       int i=1;
+       while(i<=4){
+        System.out.println("Hello, World!" + i);
+        int j=1;
+        while(j<=4){
+            System.out.println("Bye" +j);
+            j++;
+        }
+        i++;
+       }
+
+       System.out.println("Thank you" +i);
+    }
+
+}
+*/
+
+/*                                                ( do while loop )
+public class example {                                            
+    public static void main(String[] args) {
+
+       int i=1;
+       do{
+        System.out.println("Hello, World!" + i);
+        i++;
+       } while(i<=4);
+
+
+    }
+
+}
+
+*/
