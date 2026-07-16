c5a4202d61b6e2bd550f844872231ec14db810a6
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2026-07-16 21:51:10 +0530

    commit

---
+++ object/Anonymous_Object.java
@@ -0,0 +1,34 @@
+package object;
+
+
+class A
+{
+	public A()
+	{
+		System.out.println("object created");
+	}
+	public void show()
+	{
+		System.out.println("in A show");
+	}
+}
+
+
+public class Anonymous_Object
+{
+	public static void main(String a[]) 
+	{
+		int marks;
+		marks=99;
+		
+		new A();   //anonymous object
+		new A().show();
+		
+//		A obj=new A();
+		A obj;
+		obj=new A();
+		
+		obj.show();
+	}
+}
+
