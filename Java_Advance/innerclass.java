43ce8e42e6df8748a247d66a56e18737eecb9f7f
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2026-07-24 11:58:10 +0530

    commit

---
+++ Java_Advance/innerclass.java
@@ -0,0 +1,40 @@
+package Java_Advance;
+class A
+{
+	int age;
+	
+	public void show()
+	{
+		System.out.println("in show");
+	}
+	
+//	class B
+//	{
+//		public void config()
+//		{
+//			System.out.println("in config");
+//		}
+//	}
+	
+	static class B
+	{
+		public void config()
+		{
+			System.out.println("in config");
+		}
+	}
+}
+
+public class innerclass {
+    public static void main(String[] args) {
+    	A obj=new A();
+    	obj.show();
+    	
+//    	A.B obj1=obj.new B();
+//    	obj1.config();
+    	
+    	A.B obj1=new A.B();
+    	obj1.config();
+
+    }
+}
