18b82dc5105e882a6ba91b5165545ebc1319d0d6
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2026-07-25 12:59:25 +0530

    commit

---
+++ Java_Advance/need_ineterface.java
@@ -0,0 +1,46 @@
+package Java_Advance;
+
+
+interface Computer
+{
+	 void code();
+}
+
+class Laptop implements Computer
+{
+	public void code()
+	{
+		System.out.println("code, compile, run");
+	}
+
+}
+
+class Desktop implements Computer
+{
+	public void code()
+	{
+		System.out.println("code, compile, faster");
+	}
+}
+class Developer
+{
+//	public void devApp(Laptop lap)
+	public void devApp(Computer lap)
+	{
+		lap.code();
+	}
+}
+
+public class need_ineterface {
+    public static void main(String[] args) {
+//        Laptop lap=new Laptop();
+//        Desktop desk=new Desktop();
+        
+        Computer lap=new Laptop();
+        Computer desk=new Desktop();
+        
+        Developer navin=new Developer();
+        navin.devApp(lap);
+        
+    }
+}
