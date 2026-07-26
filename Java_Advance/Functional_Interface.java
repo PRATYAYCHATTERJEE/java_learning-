73e8404b05756791124a399d9230c19681921a1d
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2026-07-27 00:05:09 +0530

    commit

---
+++ Java_Advance/Functional_Interface.java
@@ -0,0 +1,32 @@
+package Java_Advance;
+
+@FunctionalInterface
+interface A
+{
+	void show();
+//	void run();
+}
+//class B implements A
+//{
+//	public void show()
+//	{
+//		System.out.println("in Show");
+//	}
+//}
+
+public class Functional_Interface {
+   public static void main(String[] args) {
+    	
+    	A obj=new A()
+    	{    	
+    		public void show()
+    		{
+    			System.out.println("in Show");
+    		}
+    	};
+ //   	A obj=new A();
+ //   	A obj=new B();
+    	obj.show();
+    }
+}
+
