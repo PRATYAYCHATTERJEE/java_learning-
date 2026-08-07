368fe17aabade7d9c2632fe752a9727f7677004d
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2026-08-07 23:50:45 +0530

    commit

--- Java_Advance/Map.java
+++ Java_Advance/Map.java
@@ -3,5 +3,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Hashtable;
 public class Map {
-    
+    public static void main(String[] args){   
+    	
+//    	Map<String, Integer> students=new HashMap<>();
+    	Map<String, Integer> students=new Hashtable<>();
 }
