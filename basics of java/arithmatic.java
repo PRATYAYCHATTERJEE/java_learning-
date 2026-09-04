42509aaad81fe99bb1846c8867fb74dbfee28e52
Pratyay Chatterjee <pratyay.ch2006@gmail.com>
2026-09-04 23:52:37 +0530

    Update y to 4 in arithmatic.java

--- basics of java/arithmatic.java
+++ basics of java/arithmatic.java
@@ -1,7 +1,7 @@
 public class arithmatic {
     public static void main(String[] args) {
         int x = 5;
-        int y = 2;
+        int y = 4;
         System.out.println(x+y);
          System.out.println(x-y);
        
