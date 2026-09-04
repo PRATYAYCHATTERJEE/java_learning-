0c7ae54ff752ec9a7c8e40c1a6ec1aa9fb247210
Pratyay Chatterjee <pratyay.ch2006@gmail.com>
2026-09-04 23:56:34 +0530

    Update x to 3 in arithmatic.java

--- basics of java/arithmatic.java
+++ basics of java/arithmatic.java
@@ -1,6 +1,6 @@
 public class arithmatic {
     public static void main(String[] args) {
-        int x = 5;
+        int x = 3;
         int y = 4;
         System.out.println(x+y);
          System.out.println(x-y);
