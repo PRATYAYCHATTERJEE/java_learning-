d03eee2aeccdf0ea623de7d02bca43be35c2e315
Pratyay Chatterjee <pratyay.ch2006@gmail.com>
2026-09-05 22:51:33 +0530

    Update x to 311 and y to 411 in arithmatic.java

--- basics of java/arithmatic.java
+++ basics of java/arithmatic.java
@@ -1,10 +1,10 @@
 public class arithmatic {
     public static void main(String[] args) {
-        int x = 3;
-        int y = 4;
+        int x = 311;
+        int y = 411;
         System.out.println(x+y);
          System.out.println(x-y);
-       
+       System.out.println(x*y);
        
         System.out.println(x/y);
     }
