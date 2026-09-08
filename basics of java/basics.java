449e349e50da4439d9e0ea71e7f7d4d0ecfaf8cf
Pratyay Chatterjee <pratyay.ch2006@gmail.com>
2026-09-08 23:54:51 +0530

    Add println statements to basics.java

--- basics of java/basics.java
+++ basics of java/basics.java
@@ -4,7 +4,10 @@ public class basics{
             System.out.println("pratyay");
             System.out.println("pratyay");
             System.out.println("pratyay");
-             
+             System.out.println("pratyay");
+               System.out.println("pratyay");
+               System.out.println("pratyay");
+               System.out.println("pratyay");
 
                
         }
