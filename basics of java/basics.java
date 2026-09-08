9b781ed774ee3866554a2817a92aef04148d2b84
Pratyay Chatterjee <pratyay.ch2006@gmail.com>
2026-09-08 23:52:55 +0530

    Update basics.java: remove extra println statements

--- basics of java/basics.java
+++ basics of java/basics.java
@@ -4,9 +4,7 @@ public class basics{
             System.out.println("pratyay");
             System.out.println("pratyay");
             System.out.println("pratyay");
-             System.out.println("pratyay");
-              System.out.println("pratyay");
-               System.out.println("pratyay");
+             
 
                
         }
