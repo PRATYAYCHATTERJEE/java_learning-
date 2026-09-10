d2328b6a725ab7d943a111733f45a35e7a9f57d7
Pratyay Chatterjee <pratyay.ch2006@gmail.com>
2026-09-10 23:48:12 +0530

    Update basics.java: remove extra println lines

--- basics of java/basics.java
+++ basics of java/basics.java
@@ -12,11 +12,7 @@ System.out.println("pratyay");
 System.out.println("pratyay");
              
                System.out.println("pratyay");
-  System.out.println("pratyay");
-  System.out.println("pratyay");
-    System.out.println("pratyay");
-  System.out.println("pratyay");
-    System.out.println("pratyay");
+  
               
               
                
