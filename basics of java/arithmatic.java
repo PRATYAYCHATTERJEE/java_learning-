7ef0bb769de977b6fc42181ce4300fab79f730ee
Pratyay Chatterjee <pratyay.ch2006@gmail.com>
2026-09-13 23:50:38 +0530

    Update basics.java: remove redundant println statements

--- basics of java/arithmatic.java
+++ basics of java/arithmatic.java
@@ -2,7 +2,7 @@ public class arithmatic {
     public static void main(String[] args) {
         int x = 311;
         int y = 411;
-        System.out.println(x+y);
+      
          System.out.println(x-y);
        System.out.println(x*y);
        
--- basics of java/basics.java
+++ basics of java/basics.java
@@ -4,10 +4,7 @@ public class basics{
             System.out.println("pratyay");
             System.out.println("pratyay");
             System.out.println("pratyay");
-             System.out.println("pratyay");
-               System.out.println("pratyay");
-               System.out.println("pratyay");
-               System.out.println("pratyay");
+            
 
 
 
