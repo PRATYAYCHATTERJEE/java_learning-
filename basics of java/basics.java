b378d733648689781fb8b78b4ecb0aa9bc82dfff
Pratyay Chatterjee <pratyay.ch2006@gmail.com>
2026-09-23 23:56:48 +0530

    Update basics.java

--- basics of java/basics.java
+++ basics of java/basics.java
@@ -14,7 +14,10 @@ public class basics{
             System.out.println("pratyay");
             System.out.println("pratyay");
             System.out.println("pratyay");
-            
+            System.out.println("pratyay");
+            System.out.println("pratyay");
+            System.out.println("pratyay");
+            System.out.println("pratyay");
            
             
              
